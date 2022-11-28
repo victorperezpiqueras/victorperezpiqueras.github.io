b1de3bf42e4f38a59aa7abf4a5654bf441f84d71
victorperezpiqueras <victorperezpiqueras@gmail.com>
2022-11-28 18:21:39 +0100

    dont show pdf on mobile as it triggers download automatically

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,4 +1,5 @@
 import { Button } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { MdFileDownload } from "react-icons/md";
 
 type ResumeProps = {
@@ -7,15 +8,37 @@ type ResumeProps = {
 
 function Resume(props: ResumeProps) {
   let cv = new URL(`../../assets/${props.cv}`, import.meta.url).href;
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
-    <div className="flex flex-col justify-center items-center space-y-4 text-black h-screen">
+    <div className="flex flex-col justify-start items-center space-y-4 text-black h-screen">
       <a href={cv} download>
         <Button>
           <MdFileDownload className="mr-2 h-5 w-5" />
           Download
         </Button>
       </a>
-      <iframe title="file" style={{ width: "60%", height: "100%" }} src={cv} />
+      {isMobile ? (
+        <></>
+      ) : (
+        <iframe
+          title="file"
+          style={{ width: "60%", height: "100%" }}
+          src={cv}
+        />
+      )}
     </div>
   );
 }
